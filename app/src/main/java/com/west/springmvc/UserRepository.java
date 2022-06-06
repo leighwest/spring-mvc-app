4924b40a57b34484532955dd4d74d65c98e1335a
Leigh West <leighwest@hotmail.com>
2022-06-06 22:34:54 +0000

    Created UserRepository interface.

---
+++ app/src/main/java/com/west/springmvc/UserRepository.java
@@ -0,0 +1,10 @@
+package com.west.springmvc;
+
+import com.west.springmvc.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findUserByEmail(String email);
+}
