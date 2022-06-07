fbdd1ca383ebddf1e85384c512a65345cae7695e
Leigh West <leighwest@hotmail.com>
2022-06-07 07:52:05 +0000

    Implemented post method for creating user.

--- app/src/main/java/com/west/springmvc/service/impl/UserServiceImpl.java
+++ app/src/main/java/com/west/springmvc/service/impl/UserServiceImpl.java
@@ -1,14 +1,33 @@
 package com.west.springmvc.service.impl;
 
+import com.west.springmvc.UserRepository;
+import com.west.springmvc.io.entity.UserEntity;
 import com.west.springmvc.service.UserService;
 import com.west.springmvc.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto user) {
-        return null;
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("test");
+        userEntity.setUserId("testUserId");
+
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails, returnValue);
+
+        return returnValue;
     }
 }
